3fbfd9146a97cc39f1d356b9fab48a0fc85ccab7
Juanjoskp <65194247+Juanjoskp@users.noreply.github.com>
2024-06-06 09:17:11 +0200

    subiendo resto de archivos

---
+++ Op. aritmeticos/Compuestos.py
@@ -0,0 +1,19 @@
+lista = ["Juanjo","Bernuy","Badosa","Mate","Quintana"]
+tupla = ("Juanjo","Bernuy","Mate","Barrio","Sanchez") 
+print(lista)
+print(lista[1])
+print(tupla)
+print(tupla[3])
+
+conjunto = ("rojo","amarillo","verde","azul","rojo")
+print(conjunto)
+
+diccionario = {
+    'Nombre' : "Juanjo",
+    'Apellido 1' : "Bernuy",
+    'Apellido 2' : "Badosa",
+    'Apeelido 3' : "Mate",
+    'Apellido 4' : "Quintana"
+}
+print(diccionario)
+print(diccionario["Nombre"])
---
+++ Op. aritmeticos/Holamundo.py
@@ -0,0 +1,17 @@
+print ("Hola mundo")
+print ("2 + 2")
+a = 3
+b = 5
+c = a + b
+print (c)
+numero = 5
+numero +=2
+print (numero)
+nombre = "Juanjo"
+saludo = "Hola "
+bienvenida = (saludo + nombre)
+print(bienvenida)
+nombre = "Juanjo"
+saludo = f"Hola {nombre} ¿Cómo estás?"
+bienvenida2 = (saludo)
+print(bienvenida2)
