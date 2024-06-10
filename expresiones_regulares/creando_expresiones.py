73fb94676333e209b024c91fd89b821173491ce3
Juanjoskp <65194247+Juanjoskp@users.noreply.github.com>
2024-06-10 09:54:41 +0200

    subida final git

--- expresiones_regulares/creando_expresiones.py
+++ expresiones_regulares/creando_expresiones.py
@@ -1,5 +1,5 @@
 import re
-texto = "el teléfono es +1 555 555-5555"
-pattern = r'\+\d{1}\s\d{3}\s\d{3}-\d{4}'
+texto = "el teléfono es +1 555 555 5555"
+pattern = r'\+\d{1}\s\d{3}\s\d{3}\s\d{4}'
 remplazo = re.sub(pattern,"(numero oculto)",texto)
 print(remplazo)
